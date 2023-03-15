ffe5e19c771dcda6378fbda11d269bc865e9babf
Oscar Garcia <oscare_gs@hotmail.com>
2023-03-15 12:50:05 -0600

    Registration of models in admin app

--- servimax/classified/admin.py
+++ servimax/classified/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Employer, Worker, Contract
+
+class EmployerAdmin(admin.ModelAdmin):
+    pass
+
+class WorkerAdmin(admin.ModelAdmin):
+    pass
+
+class ContractAdmin(admin.ModelAdmin):
+    pass
 
 # Register your models here.
+admin.site.register(Employer, EmployerAdmin)
+admin.site.register(Worker, WorkerAdmin)
+admin.site.register(Contract, ContractAdmin)
