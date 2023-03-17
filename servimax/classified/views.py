1dfe6e9ad53fd5f8c2f711db8ad784f12d3f6030
Oscar Garcia <oscare_gs@hotmail.com>
2023-03-16 19:14:52 -0600

    First implementation of Worker data displayed in Index of classified

--- servimax/classified/views.py
+++ servimax/classified/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+from .models import Worker
 
 # Create your views here.
-def index(request):
-    return HttpResponse('Hello World!')
+class IndexView(generic.ListView):
+    template_name = 'classified/index.html'
+    context_object_name = 'workers_list'
+
+    def get_queryset(self):
+        return Worker.objects.all()
